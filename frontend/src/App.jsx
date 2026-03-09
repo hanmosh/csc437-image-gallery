a6f7d4a6896bec33e84c3893eb20d50c28f3f081
hannah <113932134+hanmosh@users.noreply.github.com>
2026-03-08 21:48:25 -0700

    lab 18 and 19

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,32 @@
+import { Routes, Route } from "react-router";
 import { AllImages } from "./images/AllImages.jsx";
 import { ImageDetails } from "./images/ImageDetails.jsx";
 import { UploadPage } from "./UploadPage.jsx";
 import { LoginPage } from "./LoginPage.jsx";
+import { MainLayout } from "./MainLayout.jsx";
+import { VALID_ROUTES } from "./shared/ValidRoutes.js";
+
+function NotFound() {
+    return (
+        <>
+            <h2>Page not found</h2>
+            <p>Try one of the links above.</p>
+        </>
+    );
+}
 
 function App() {
-    const POSSIBLE_PAGES = [
-        <AllImages />,
-        <ImageDetails imageId={"0"} />,
-        <UploadPage />,
-        <LoginPage />
-    ];
-
-    return POSSIBLE_PAGES[0];
+    return (
+        <Routes>
+            <Route path={VALID_ROUTES.HOME} element={<MainLayout />}>
+                <Route index element={<AllImages />} />
+                <Route path={VALID_ROUTES.IMAGE_DETAILS} element={<ImageDetails />} />
+                <Route path={VALID_ROUTES.UPLOAD} element={<UploadPage />} />
+                <Route path={VALID_ROUTES.LOGIN} element={<LoginPage />} />
+                <Route path="*" element={<NotFound />} />
+            </Route>
+        </Routes>
+    );
 }
 
 export default App;
--- frontend/src/MainLayout.jsx
+++ frontend/src/MainLayout.jsx
@@ -1,12 +1,12 @@
-import React from "react";
+import { Outlet } from "react-router";
 import { Header } from "./Header.jsx";
 
-export function MainLayout(props) {
+export function MainLayout() {
     return (
         <div>
             <Header />
-            <div style={{padding: "0 2em"}}>
-                {props.children}
+            <div style={{ padding: "0 2em" }}>
+                <Outlet />
             </div>
         </div>
     );
